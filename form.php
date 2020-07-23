67875f497589aa8007b14686303131696498c4e5
Kalebe Gouvea <k.gouvea@live.com>
2020-07-23 17:13:27 -0300

    Teste das variaveis utilizando echo

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
 $nome = $snome = $email = $pwd = $fone = $bday = $sex = $end = $estado = $cidade = $ofertas = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["nome"]);
+  $nome = test_input($_POST["nome"]);
   $snome = test_input($_POST["snome"]);
   $email = test_input($_POST["email"]);
   $pwd = test_input($_POST["pwd"]);
@@ -25,14 +25,14 @@ function test_input($data) {
 }
 // Fim da validação das variáveis POST
 
-echo $_POST["nome"]. "<br>";
-echo $_POST["snome"]. "<br>";
-echo $_POST["email"]. "<br>";
-echo $_POST["pwd"]. "<br>";
-echo $_POST["fone"]. "<br>";
-echo $_POST["bday"]. "<br>";
-echo $_POST["sex"]. "<br>";
-echo $_POST["end"]. "<br>";
-echo $_POST["estado"]. "<br>";
-echo $_POST["cidade"]. "<br>";
-echo $_POST["ofertas"]. "<br>";
+echo $nome. "<br>";
+echo $snome. "<br>";
+echo $email. "<br>";
+echo $pwd. "<br>";
+echo $fone. "<br>";
+echo $bday. "<br>";
+echo $sex. "<br>";
+echo $end. "<br>";
+echo $estado. "<br>";
+echo $cidade. "<br>";
+echo $ofertas. "<br>";
